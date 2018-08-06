6444d7ecaa699a8e41329f01c2386e097b6d438e
David Arribas <david.arribascanalejo@unisys.com>
2018-08-06 12:13:35 +0200

    IOS

---
+++ DavidUpsIOSProject/DavidUpsIOSProject/WellcomeController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  DavidUpsIOSProject
+//
+//  Created by David Arribas on 6/8/18.
+//  Copyright © 2018 David Arribas. All rights reserved.
+//
+
+import UIKit
+
+class WellcomeControlet: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func colorBlue() {
+        view.backgroundColor = .cyan
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
